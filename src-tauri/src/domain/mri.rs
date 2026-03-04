792454ebc0b8afb25f7429916776f8bb60b47d07
Adam Aske <adamaske@live.no>
2026-03-04 17:49:14 +0100

    Add MRI domain file with usage notes

---
+++ src-tauri/src/domain/mri.rs
@@ -0,0 +1,26 @@
+
+
+// Use interaction:
+// 1. The user provides a .nii.gz
+// ----> and a .snirf file
+// 2. FreeSurfer segmentation runs
+// 3. The resulting meshes are loaded into NIRWizard
+// 4. The user manually aligns the fNIRS probe
+// 5. The user presses "Calculate Sensitivity"
+// 6. The sensitivity profile is calculated and visualized
+// 7. Projection mode becomes available letting the user
+// set a timepoint in the DataPlotter chart.
+// 
+
+
+// We need some install procedures
+// We need a license, input and output folder
+// for FreeSurfer
+// We need these folder accesible at runtime, maybe
+// enviroment variable is the easiest
+// settup_enviromnet_varriable.bat
+
+
+
+
+
